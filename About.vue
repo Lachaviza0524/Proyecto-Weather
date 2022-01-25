7645746fce81a28146a9291c91812a881fe28bfe
Andres Daniel <90276838+Lachaviza0524@users.noreply.github.com>
2022-01-25 00:06:12 -0500

    Add files via upload
    First Program with Vue and AXIOS

---
+++ About.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="result" class="about">
+    <h1>Weather</h1>
+    <header>
+      <p>
+        <b> {{ result.name }} </b>
+      </p>
+    </header>
+
+    <img
+      :src="
+        'http://openweathermap.org/img/w/' + result.weather[0].icon + '.png'
+      "
+      alt=" "
+    />
+    <p>{{ result.weather[0].description }}</p>
+    <p>Temp: {{ result.main.temp }}</p>
+    <p>Degrees: {{ result.wind.deg }}</p>
+  </div>
+</template>
+<style></style>
+<script>
+import axios from "axios";
+export default {
+  data: () => ({
+    result: null,
+  }),
+  created() {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=mazatlan&APPID=312f667be3a4279a5707948323b672bc"
+      )
+      .then((result) => {
+        this.result = result.data;
+        console.log(result.data);
+      });
+  },
+};
+</script>
